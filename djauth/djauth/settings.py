68ba10cbf9f0f6e86fd1e0ac5ba841af1bed45a0
Sofyan Ajridi <sofyanajridi@Sofyans-MBP.home>
2018-10-21 18:42:23 +0200

    New version

--- djauth/djauth/settings.py
+++ djauth/djauth/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'djauth'
+    
 ]
 
 MIDDLEWARE = [
@@ -117,8 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
